b3311f4d6890d48377552947ea79f50e4029a8f8
Gokac9 <theabraham9@gmail.com>
2025-05-08 15:00:33 +0200

    sync

--- todopintura_pos_line_ubication/__manifest__.py
+++ todopintura_pos_line_ubication/__manifest__.py
@@ -16,6 +16,8 @@
         'point_of_sale._assets_pos': [
             'todopintura_pos_line_ubication/static/src/js/control_buttons.js',
             'todopintura_pos_line_ubication/static/src/xml/control_buttons.xml',
+            'todopintura_pos_line_ubication/static/src/js/action_pad.js',
+            'todopintura_pos_line_ubication/static/src/xml/action_pad.xml',
             'todopintura_pos_line_ubication/static/src/js/location_line_dialog.js',
             'todopintura_pos_line_ubication/static/src/xml/location_line_dialog.xml',
             'todopintura_pos_line_ubication/static/src/js/pos_order_line.js',
