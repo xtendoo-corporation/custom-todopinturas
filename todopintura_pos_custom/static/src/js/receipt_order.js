909923197e13cd6d23fc435c56cd82e295bd189d
Gokac9 <theabraham9@gmail.com>
2025-06-04 10:34:44 +0200

    fix pos order receipt company city

--- todopintura_pos_custom/static/src/js/receipt_order.js
+++ todopintura_pos_custom/static/src/js/receipt_order.js
@@ -2,10 +2,18 @@
 import { patch } from "@web/core/utils/patch";
 import { ReceiptHeader } from "@point_of_sale/app/screens/receipt_screen/receipt/receipt_header/receipt_header";
 import { _t } from "@web/core/l10n/translation";
+import { onMounted } from "@odoo/owl";
 
-patch(ReceiptHeader, {
+patch(ReceiptHeader.prototype, {
     get vatText() {
         // Se retorna "CIF: ..." en lugar de "Tax ID: ..."
         return _t("CIF: %(vatId)s", { vatId: this.props.data.company.vat });
+    },
+    setup() {
+        // En lugar de usar this._super, implementamos directamente
+        onMounted(() => {
+            console.log("Datos de la compañía:", this.props.data.company);
+            console.log("Ciudad:", this.props.data.company.city);
+        });
     },
 });
