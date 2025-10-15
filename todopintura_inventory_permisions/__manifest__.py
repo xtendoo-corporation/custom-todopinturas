b2aa85e7a175af6b8b47e1f449464fe18b664a1e
Gokac9 <theabraham9@gmail.com>
2025-10-15 12:55:00 +0200

    add module todopintura_inventory_permisions

---
+++ todopintura_inventory_permisions/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Todopintura Inventory Permissions',
+    'version': '19.0.1.0.0',
+    'category': 'Inventory',
+    'summary': 'Permisos de inventario para Tienda 2',
+    'author': 'Abraham (Xtendoo)',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'data/groups.xml',
+        'data/security.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
