b42c1471fca292635daeae02cba49476e7bb1cdb
Gokac9 <theabraham9@gmail.com>
2026-04-27 12:27:46 +0200

    sync

--- todopintura_extend_pos_conventional/__manifest__.py
+++ todopintura_extend_pos_conventional/__manifest__.py
@@ -19,7 +19,7 @@
         'views/res_config_settings_views.xml',
         'views/res_partner_views.xml',
         'views/pos_order_views.xml',
-        'views/pos_payment_views.xml',
+        #'views/pos_payment_views.xml',
         'views/credit_override_wizard_views.xml',
         'report/albaran_receipt.xml',
     ],
