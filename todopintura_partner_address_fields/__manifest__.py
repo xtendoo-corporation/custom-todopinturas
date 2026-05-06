6b14f380565c97be5622246652b1f9e1a45fe779
EndikaPM <endika.perez.mas@gmail.com>
2026-05-06 07:20:27 +0200

    add module todopinturas_partner_addres_fiels

---
+++ todopintura_partner_address_fields/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Todopintura Partner Address Fields",
+    "summary": "Hace obligatorios los campos Dirección y Población en contactos.",
+    "description": """
+        Extiende contactos para hacer obligatorios los campos estándar
+        de dirección (street) y población (city).
+    """,
+    "author": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Contacts",
+    "version": "19.0.1.0.0",
+    "depends": ["contacts"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+}
+
---
+++ todopintura_partner_address_fields/models/res_partner.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Este addon solo hereda vistas para hacer obligatorios
+# los campos estándar de dirección y población.
+
